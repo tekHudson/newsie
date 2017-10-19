fb7bcc260238132536f9c92e6fa64fc40d4d1a6b
Tek Hudson <tek@reflektive.com>
2017-10-19 09:45:54 -0700

    Working with article posting and articles on welcome index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,25 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def show
+     @article = Article.find(params[:id])
+  end
+
   def new
   end
 
   def create
-    render plain: params[:article].inspect
+    @article = Article.new(permitted_new_article_params)
+
+    @article.save!
+    redirect_to @article
+  end
+
+  private
+
+  def permitted_new_article_params
+    params.require(:article).permit(:title, :text)
   end
 end
