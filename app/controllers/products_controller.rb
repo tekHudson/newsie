6ea7dee185582e19c82b914966bfa402d6944d71
Tek Hudson <tek@reflektive.com>
2017-11-17 09:59:45 -0800

    Add product nave and stying for ease of use

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,9 @@
 class ProductsController < ApplicationController
   def index
     @categories = Category.all
-    @products = Product.all
     @category = Category.find(params[:category_id]) if params[:category_id].present?
+
+    @products = Product.for_category(params[:category_id])
+    @product = Product.find(params[:product_id]) if params[:product_id].present?
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,12 @@
 class Product < ApplicationRecord
   belongs_to :category
   has_one :image
+
+  def self.for_category(category_id)
+    if category_id.present?
+      where(category_id: category_id)
+    else
+      all
+    end
+  end
 end
